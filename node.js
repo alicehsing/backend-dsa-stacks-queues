6a282ef5ccfee8eb44e9aa06b5a620a25c4ce564
Alice Hsing <alicehsing@gmail.com>
2022-04-05 18:27:41 -0700

    implemented class BinaryTreeNode

--- node.js
+++ node.js
@@ -30,3 +30,102 @@ const nodeB = new LinkedListNode('B');
 root.add(nodeB);
 root.add(new LinkedListNode('C'));
 console.log(root.getList()); 
+
+
+
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  add(node) {
+    console.log('NODE VALUE: ', node.value, this.value, node.value < this.value);
+
+    if(node.value < this.value) {
+      this.left ? this.left.add(node) : this.left = node;  
+    } else {
+      this.right ? this.right.add(node) : this.right = node;
+    }
+  }
+
+}
+
+const B = new BinaryTreeNode('B');
+const A = new BinaryTreeNode('A');
+const C = new BinaryTreeNode('C');
+const D = new BinaryTreeNode('D');
+
+// B will be the root of the tree:
+B.add(A);
+B.add(D);
+B.add(C);
+
+console.log(B);
+
+
+class PersonTreeNode {
+  constructor(person) {
+    this.value = person.name;
+    this.person = person;
+    this.left = null;
+    this.right = null;
+  }
+  
+  add(node) {
+    if(node.value < this.value) {
+      this.left ? this.left.add(node) : this.left = node;
+    } else {
+      this.right ? this.right.add(node) : this.right = node;
+    }
+  }
+
+  //   findPerson(name) {
+  //     // is it me you are looking for?
+  //     if(this.value === name) return this.person;
+
+  //     // which direction to go?
+  //     const direction = node.value < this.value ? 'left' : 'right';
+
+  //     //out of nodes, doesn't exist, return null
+  //     if(!this[direction]) return null;
+    
+  //     // delegate to the left or right
+  //     return this[direction].findPerson(name);
+        
+//   }
+}
+
+
+const Ba = new PersonTreeNode({
+  name: 'cathy',
+  phone: '503-000-3333',
+  address: '123 abc st'
+});
+
+const Aa = new PersonTreeNode({
+  name: 'jack',
+  phone: '503-000-3332',
+  address: '120 abc st'
+});
+
+const Ca = new PersonTreeNode({
+  name: 'polly',
+  phone: '503-000-3335',
+  address: '127 abc st'
+});
+
+const Da = new PersonTreeNode({
+  name: 'nick',
+  phone: '503-000-3331',
+  address: '121 abc st'
+});
+
+Ba.add(Aa);
+Ba.add(Da);
+Ba.add(Ca);
+// Ba.findPerson('jack');
+
+
+console.log(JSON.stringify(Ba, true, 2));
